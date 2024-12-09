be01840b12247b2c16e254173add6e1e412d9085
scapula07 <bartholomewonogwu@yahoo.com>
2024-12-09 18:38:10 +0100

    home page

---
+++ src/components/Home/banner.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { IoLogoWordpress } from "react-icons/io5";
+import { FaShopify } from "react-icons/fa";
+
+export default function Banner() {
+  return (
+    <div className='w-full flex flex-col items-center py-20 space-y-8'>
+         <div className='w-full flex flex-col items-center'>
+             <h5 className='font-bold text-2xl'>Over 10k Organizations Trust in our Seamless Integration with</h5>
+         </div>   
+         <div className='w-full flex items-center justify-center space-x-20'>
+              {
+                [
+                 {
+                    icon:<IoLogoWordpress />,
+                    text:'Shopify',
+                    img:'/assets/shopify.png'
+
+                 },
+                 {
+                    icon:<FaShopify />,
+                    text:'Shopify',
+                    img:'/assets/woocommerce.png'
+                 },
+                 {
+                    icon:<FaShopify />,
+                    text:'Shopify',
+                    img:'/assets/wordpress.png'
+                 },
+                 {
+                    icon:<FaShopify />,
+                    text:'Livepeer',
+                    img:'/assets/storj.png'
+                 }
+
+                 ]?.map((item)=>{
+                    return(
+                      <div className='flex space-x-2'>
+                         <img src={item.img} />
+                      </div>
+                    )
+                })}
+          </div>
+    </div>
+  )
+}
